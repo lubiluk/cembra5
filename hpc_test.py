16db53baf67edbf4337c9ab06a9294f1b50aa142
Pawel Gajewski <lubiluk@gmail.com>
2021-12-13 16:27:22 +0100

    hpc test

--- hpc_test.py
+++ hpc_test.py
@@ -1,14 +1,48 @@
+# import ray
+# import cma
+
+# @ray.remote
+# def evaluate(x):
+#     return cma.ff.rosen(x)
+
+# es = cma.CMAEvolutionStrategy(12 * [0], 0.5)
+# while not es.stop():
+#     solutions = es.ask()
+#     remotes = [evaluate.remote(x) for x in solutions]
+#     es.tell(solutions, ray.get(remotes))
+#     es.logger.add()  # write data to disc to be plotted
+#     es.disp()
+
+
+# trainer.py
+from collections import Counter
+import os
+import socket
+import sys
+import time
 import ray
-import cma
+
+num_cpus = 5
+
+ray.init(address=os.environ["ip_head"])
+
+print("Nodes in the Ray cluster:")
+print(ray.nodes())
+print("----------------")
+
 
 @ray.remote
-def evaluate(x):
-    return cma.ff.rosen(x)
+def f():
+    time.sleep(1)
+    # ~ print("x")
+    return socket.gethostbyname(socket.gethostname())
 
-es = cma.CMAEvolutionStrategy(12 * [0], 0.5)
-while not es.stop():
-    solutions = es.ask()
-    remotes = [evaluate.remote(x) for x in solutions]
-    es.tell(solutions, ray.get(remotes))
-    es.logger.add()  # write data to disc to be plotted
-    es.disp()
+
+# The following takes one second (assuming that
+# ray was able to access all of the allocated nodes).
+for i in range(4):
+    start = time.time()
+    ip_addresses = ray.get([f.remote() for _ in range(num_cpus)])
+    print(Counter(ip_addresses))
+    end = time.time()
+    print(end - start)
